2150597c9523ce353c5f2171a8c15c90b290e62f
Mitchell R <support@bettercorp.co.za>
2021-09-05 11:55:32 +0200

    Starting CI/CD build for web docs

--- docs/.vuepress/.temp/pages/docs/hardware/index.html.vue
+++
@@ -1,5 +0,0 @@
-<template><h1 id="hardware" tabindex="-1"><a class="header-anchor" href="#hardware" aria-hidden="true">#</a> Hardware</h1>
-<div class="custom-container tip"><p class="custom-container-title">TODO</p>
-<p>Content to be created for this page</p>
-</div>
-</template>
