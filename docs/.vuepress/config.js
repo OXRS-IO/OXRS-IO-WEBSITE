023bda0d59aaf8022bb67715c1f3f3d4623bbb41
Aaron Knox <aaron_knox@me.com>
2021-09-13 07:07:12 +0100

    Added OXRS GitHub link in main navigation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -90,6 +90,10 @@ module.exports = {
           },
         ],
       },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/OXRS-IO/',
+      },
     ],
     sidebar: {
       '/docs/hardware': [
