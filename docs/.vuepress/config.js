18f901ca6511c36a3d2cbbb9a06ffe6f106b8d7e
Aaron Knox <64750327+MakerDockio@users.noreply.github.com>
2021-12-16 21:56:57 +0000

    Added new template page and updated navigation for new State IO Firmware - #37 - https://github.com/OXRS-IO/OXRS-IO-WEBSITE/issues/37

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -362,6 +362,7 @@ module.exports = {
               children: [
                   '/docs/firmware/state-monitor-esp32.md',
                   '/docs/firmware/state-controller-esp32.md',
+                  '/docs/firmware/state-io-esp32.md',
                   '/docs/firmware/smoke-detector-esp32.md',
                   '/docs/firmware/led-controller-esp32.md',
               ],
