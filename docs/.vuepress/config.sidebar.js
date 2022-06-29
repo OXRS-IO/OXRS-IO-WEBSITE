3a03fe4c206bdb6662accb4a6b623b6c094fe466
Mitchell R <support@bettercorp.co.za>
2022-06-29 16:40:36 +0200

    Added validation for nav and sidebar, cleanups, updates to build and validation. General updates for build

---
+++ docs/.vuepress/config.sidebar.js
@@ -0,0 +1,189 @@
+module.exports = {
+  "/guides": [
+    {
+      text: "Getting Started",
+      link: "/guides/getting-started",
+      // children: [{
+      //     text: 'Getting Started 2',
+      //     // link: '/guides/getting-started/getting-started',
+      //   },
+    },
+    {
+      text: "Advanced",
+      link: "/guides/advanced",
+      children: [
+        {
+          text: "Home Assistant",
+          link: "/guides/advanced/home_assistant",
+        },
+        {
+          text: "Node-RED",
+          link: "/guides/advanced/node_red",
+        },
+        {
+          text: "Backup/Restore",
+          link: "/guides/advanced/backup-restore",
+        },
+      ],
+    },
+    {
+      text: "Standards",
+      link: "/guides/standards/README",
+      children: [
+        {
+          text: "IDC",
+          link: "/guides/standards/IDC",
+        },
+        {
+          text: "IO Ethernet",
+          link: "/guides/standards/IO-ethernet",
+        },
+        {
+          text: "Versioning",
+          link: "/guides/standards/versioning",
+        },
+      ],
+    },
+    {
+      text: "Writing Docs",
+      link: "/guides/writing-docs",
+    },
+  ],
+  "/add-ons": [
+    {
+      text: "Accessibility",
+      link: "/add-ons/accessibility/README",
+      children: ["/add-ons/accessibility/ac-tower-controller"],
+    },
+    {
+      text: "Displays",
+      link: "/add-ons/displays",
+      children: [
+        "/add-ons/displays/IPS-display",
+        "/add-ons/displays/128x32-OLED-display",
+        "/add-ons/displays/WT32-SC01-display",
+      ],
+    },
+    // {
+    //   text: 'Relays',
+    //   link: '/add-ons/relays',
+    // },
+    // {
+    //   text: 'Sensors',
+    //   link: '/add-ons/sensors',
+    // },
+    {
+      text: "Switches",
+      link: "/add-ons/switches",
+      children: ["/add-ons/switches/ac-smart-switch"],
+    },
+  ],
+  "/docs": [
+    {
+      text: "Hardware",
+      link: "/docs/hardware/README",
+      children: [
+        {
+          text: "Controllers",
+          children: [
+            "/docs/hardware/controllers/rack32",
+            "/docs/hardware/controllers/room8266",
+            "/docs/hardware/controllers/rack-fan-controller",
+          ],
+        },
+        {
+          text: "Input Devices",
+          children: [
+            "/docs/hardware/input-devices/I2CRJ45",
+            "/docs/hardware/input-devices/CBUS-to-RJ45",
+            "/docs/hardware/input-devices/rotary-encode-to-rj45",
+            "/docs/hardware/input-devices/rotary-encode",
+            //                   '/docs/hardware/input-devices/hardware-template',
+          ],
+        },
+        {
+          text: "Output Devices",
+          children: [
+            "/docs/hardware/output-devices/pwm-controllers",
+            "/docs/hardware/output-devices/DIN-Relay-Driver-4ch",
+            //                   '/docs/hardware/output-devices/hardware-template',
+          ],
+        },
+        {
+          text: "Input & Output Devices",
+          children: [
+            "/docs/hardware/input-output-devices/smoke-detector-sd-16port",
+            "/docs/hardware/input-output-devices/SenseRJ45",
+          ],
+        },
+        {
+          text: "Shields",
+          children: ["/docs/hardware/shields/rack32-knx-shield"],
+        },
+      ],
+    },
+    {
+      text: "Firmware",
+      link: "/docs/firmware/README",
+      children: [
+        {
+          text: "ESP32",
+          children: [
+            "/docs/firmware/state-monitor-esp32",
+            "/docs/firmware/state-controller-esp32",
+            "/docs/firmware/state-io-esp32",
+            "/docs/firmware/smoke-detector-esp32",
+            // '/docs/firmware/led-controller-esp32',
+          ],
+        },
+      ],
+    },
+    {
+      text: "Libraries",
+      link: "/docs/libraries/README",
+      children: [
+        {
+          text: "ESP32",
+          children: [
+            "/docs/libraries/esp32-mqtt-library",
+            "/docs/libraries/esp32-api-library",
+            "/docs/libraries/esp32-lcd-library",
+            "/docs/libraries/esp32-io-handler-library",
+          ],
+        },
+        {
+          text: "ESP",
+          children: ["/docs/libraries/esp-sensor-library"],
+        },
+      ],
+    },
+    {
+      text: "Cases",
+      link: "/docs/cases/README",
+      children: [
+        {
+          text: "Rack Mount",
+          children: ["/docs/cases/rack-mount/rack-mount"],
+        },
+        {
+          text: "3D Printed",
+          children: ["/docs/cases/3D-printed/README"],
+        },
+      ],
+    },
+  ],
+  "/glossary": [
+    {
+      text: "SKU's",
+      link: "/glossary/skus",
+    },
+    {
+      text: "Makers",
+      link: "/glossary/makers",
+    },
+    {
+      text: "Acronyms",
+      link: "/glossary/acronyms",
+    },
+  ],
+};
---
+++ docs/.vuepress/validate.js
@@ -0,0 +1,146 @@
+const Tools = require("@bettercorp/tools").Tools;
+const navbar = require("./config.nav");
+const sidebar = require("./config.sidebar");
+const fs = require("fs");
+const path = require("path");
+
+const validateNavItemLink = (nav, baseBath = "") => {
+  let text = "Unknown",
+    pathUi = "Unknown";
+  if (typeof nav === "string") {
+    text = nav;
+    pathUi = nav;
+  } else if (Tools.isNullOrUndefined(nav.link)) {
+    return "Nav link is null or undefined.... this is weird...";
+  } else {
+    text = nav.text;
+    pathUi = nav.link;
+  }
+  console.log(` ⤤ Validate: ${text} -> ${pathUi}`);
+
+  if (pathUi.indexOf("/") !== 0) {
+    if (pathUi.indexOf("https://") === 0) return true;
+    return `Nav ${text} has an invalid link url, only https://* are supported`;
+  }
+
+  if (pathUi.toLowerCase().endsWith(".md")) {
+    return `Link ${pathUi} should not end with .md or /`;
+  }
+  if (pathUi.toLowerCase().endsWith("/")) {
+    return `Link ${pathUi} should not end with .md or /`;
+  }
+
+  const corePath = path.join(__dirname, `..${baseBath}${pathUi}`);
+  const defaultMdPath = path.join(corePath, "./README.md");
+  const directFilePath = `${corePath}.md`;
+  if (fs.existsSync(defaultMdPath)) {
+    console.log(`   -> ${defaultMdPath} : Folder path`);
+    return true;
+  }
+  if (fs.existsSync(directFilePath)) {
+    console.log(`   -> ${directFilePath} : Direct path`);
+    return true;
+  }
+  if (fs.existsSync(corePath)) {
+    console.log(`   -> ${directFilePath} : Base path`);
+    return true;
+  }
+  return `${pathUi} doesn't exist on the file system. test(${corePath})`;
+};
+const validateNavItem = (nav) => {
+  if (typeof nav === "string") {
+    console.log(`| Validate: ${nav} : Path`);
+    return validateNavItemLink(nav);
+  }
+  if (typeof nav.text !== "string") {
+    return "There is a nav item without a text property... please add one.";
+  }
+  console.log(`| Validate: ${nav.text}`);
+  if (nav.text.length < 2) {
+    return "The nav text length needs to be longer than 2 characters";
+  }
+  if (nav.text.length > 30) {
+    return "The nav text length needs to be shorter than 30 characters";
+  }
+  const whatItShouldLookLike = Tools.autoCapitalizeWords(nav.text);
+  if (nav.text !== whatItShouldLookLike) {
+    return `The nav text needs to be formatted correctly: ${whatItShouldLookLike}`;
+  }
+
+  if (
+    Tools.isNullOrUndefined(nav.link) &&
+    Tools.isNullOrUndefined(nav.children)
+  )
+    return `${nav.text} does not have link or children`;
+
+  if (!Tools.isNullOrUndefined(nav.link)) {
+    let linkVal = validateNavItemLink(nav);
+    if (linkVal !== true) return linkVal;
+  }
+
+  if (!Tools.isNullOrUndefined(nav.children)) {
+    for (let navChild of nav.children) {
+      const validationResult = validateNavItem(navChild);
+      if (validationResult !== true) {
+        return validationResult;
+      }
+    }
+  }
+
+  return true;
+};
+
+console.log("--------------------------------");
+console.log("NAV : Running validation");
+for (let nav of navbar) {
+  const validationResult = validateNavItem(nav);
+  if (validationResult !== true) {
+    console.error("");
+    console.error("");
+    console.error("++++++++++++++++++++++++++++++++++");
+    console.error("");
+    console.error(validationResult);
+    console.error("");
+    console.error("++++++++++++++++++++++++++++++++++");
+    console.error("");
+    console.error("");
+    process.exit(10);
+  }
+}
+console.log("NAV : Running validation : OKAY");
+console.log("--------------------------------");
+
+console.log("--------------------------------");
+console.log("SIDEBAR : Running validation");
+for (let nav of Object.keys(sidebar)) {
+  const validationResult = validateNavItemLink(nav);
+  if (validationResult !== true) {
+    console.error("");
+    console.error("");
+    console.error("++++++++++++++++++++++++++++++++++");
+    console.error("");
+    console.error(validationResult);
+    console.error("");
+    console.error("++++++++++++++++++++++++++++++++++");
+    console.error("");
+    console.error("");
+    process.exit(10);
+  }
+  for (let inav of sidebar[nav]) {
+    const ivalidationResult = validateNavItem(inav);
+    if (ivalidationResult !== true) {
+      console.error("");
+      console.error("");
+      console.error("++++++++++++++++++++++++++++++++++");
+      console.error("");
+      console.error(ivalidationResult);
+      console.error("");
+      console.error("++++++++++++++++++++++++++++++++++");
+      console.error("");
+      console.error("");
+      process.exit(10);
+    }
+  }
+}
+console.log("SIDEBAR : Running validation : OKAY");
+console.log("--------------------------------");
